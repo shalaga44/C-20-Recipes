0deedb3bb03e13e8b17aa300816a9ae38927f462
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-08-04 19:42:30 +0200

    init commit

---
+++ Recipes 1-5/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by shalaga44 on 8/4/20.
+//
+#include <iostream>
+#include <string>
+
+int main() {
+    using namespace std::string_literals;
+    auto output = "Hello World!"s;
+    std::cout << output << std::endl;
+    return 0;
+}
