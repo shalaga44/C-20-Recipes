90a3d3ae8399bc44477892549df259ea253b0701
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-08-04 20:46:57 +0200

    Constructing Vector Objects

--- Recipe 2-2/main.cpp
+++ Recipe 2-2/main.cpp
@@ -12,5 +12,7 @@ int main() {
     cout << vectorA.size() << " " << vectorA[0] << endl;
     MyVector vectorB(1, 10);
     cout << vectorB.size() << " " << vectorB[0] << endl;
+    MyVector vectorC{1, 10};
+    cout << vectorC.size() << " " << vectorC[0] << endl;
     return 0;
 }
